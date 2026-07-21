9f08b16ecb5132abb5942cf73297b2a6a1fe2123
sy220284 <2648897872@qq.com>
2026-07-21 08:53:40 +0800

    feat(M3-06): export state proposal contracts

--- packages/contracts/src/public-index.ts
+++ packages/contracts/src/public-index.ts
@@ -2,3 +2,4 @@ export * from './index.js';
 export * from './continuity.js';
 export * from './continuity-control.js';
 export * from './narrative-planning.js';
+export * from './state-proposal.js';

d5c42b7ab9a809078fc76a09648bdde37aab5fd9
sy220284 <2648897872@qq.com>
2026-07-31 15:20:10 +0800

    test: activate Microsoft Pinyin through TSF session API

--- tests/e2e/m8-07-windows-ime-native.ps1
+++ tests/e2e/m8-07-windows-ime-native.ps1
@@ -19,6 +19,37 @@ using System;
 using System.Runtime.InteropServices;
 
 public static class WorldForgeNativeIme {
+    private const uint CLSCTX_INPROC_SERVER = 0x1;
+    private const uint TF_PROFILETYPE_INPUTPROCESSOR = 0x1;
+    private const uint TF_IPPMF_ENABLEPROFILE = 0x1;
+    private const uint TF_IPPMF_DONTCARECURRENTINPUTLANGUAGE = 0x4;
+    private const uint TF_IPPMF_FORSESSION = 0x20000000;
+
+    private static readonly Guid ClsidInputProcessorProfiles = new Guid("33C53A50-F456-4884-B049-85FD643ECFED");
+    private static readonly Guid IidInputProcessorProfileMgr = new Guid("71C6E74C-0F28-11D8-A82A-00065B84435C");
+    private static readonly Guid ClsidMicrosoftPinyin = new Guid("81D4E9C9-1D3B-41BC-9E6C-4B40BF79E35E");
+    private static readonly Guid ProfileMicrosoftPinyin = new Guid("FA550B04-5AD7-411F-A5AC-CA038EC515D7");
+
+    [UnmanagedFunctionPointer(CallingConvention.Winapi)]
+    private delegate int ActivateProfileDelegate(
+        IntPtr self,
+        uint profileType,
+        ushort languageId,
+        ref Guid textServiceClsid,
+        ref Guid profileGuid,
+        IntPtr keyboardLayout,
+        uint flags
+    );
+
+    [DllImport("ole32.dll")]
+    private static extern int CoCreateInstance(
+        ref Guid classId,
+        IntPtr outerUnknown,
+        uint classContext,
+        ref Guid interfaceId,
+        out IntPtr interfacePointer
+    );
+
     [DllImport("user32.dll")]
     public static extern bool SetForegroundWindow(IntPtr hWnd);
 
@@ -39,6 +70,47 @@ public static class WorldForgeNativeIme {
 
     [DllImport("user32.dll")]
     public static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, UIntPtr dwExtraInfo);
+
+    public static int ActivateMicrosoftPinyin() {
+        IntPtr profileManager;
+        Guid classId = ClsidInputProcessorProfiles;
+        Guid interfaceId = IidInputProcessorProfileMgr;
+        int createResult = CoCreateInstance(
+            ref classId,
+            IntPtr.Zero,
+            CLSCTX_INPROC_SERVER,
+            ref interfaceId,
+            out profileManager
+        );
+        if (createResult < 0) {
+            Marshal.ThrowExceptionForHR(createResult);
+        }
+
+        try {
+            IntPtr virtualTable = Marshal.ReadIntPtr(profileManager);
+            IntPtr activatePointer = Marshal.ReadIntPtr(virtualTable, IntPtr.Size * 3);
+            ActivateProfileDelegate activate =
+                Marshal.GetDelegateForFunctionPointer<ActivateProfileDelegate>(activatePointer);
+            Guid textServiceClsid = ClsidMicrosoftPinyin;
+            Guid profileGuid = ProfileMicrosoftPinyin;
+            uint flags =
+                TF_IPPMF_FORSESSION |
+                TF_IPPMF_ENABLEPROFILE |
+                TF_IPPMF_DONTCARECURRENTINPUTLANGUAGE;
+            return activate(
+                profileManager,
+                TF_PROFILETYPE_INPUTPROCESSOR,
+                0x0804,
+                ref textServiceClsid,
+                ref profileGuid,
+                IntPtr.Zero,
+                flags
+            );
+        }
+        finally {
+            Marshal.Release(profileManager);
+        }
+    }
 }
 '@
 
@@ -101,6 +173,11 @@ function Capture-Desktop {
   }
 }
 
+$activationHresult = [WorldForgeNativeIme]::ActivateMicrosoftPinyin()
+if ($activationHresult -ne 0) {
+  throw ('M8_07_WINDOWS_IME_TSF_ACTIVATION_FAILED: 0x{0:X8}' -f ([uint32]$activationHresult))
+}
+
 $windowHandle = Get-ElectronWindowHandle -RootProcessId $ElectronProcessId
 [WorldForgeNativeIme]::ShowWindow($windowHandle, 9) | Out-Null
 [WorldForgeNativeIme]::SetForegroundWindow($windowHandle) | Out-Null
@@ -111,7 +188,7 @@ if ($chineseLayout -eq [IntPtr]::Zero) {
   throw 'M8_07_WINDOWS_IME_LAYOUT_LOAD_FAILED'
 }
 [WorldForgeNativeIme]::PostMessage($windowHandle, 0x0050, [IntPtr]::Zero, $chineseLayout) | Out-Null
-Start-Sleep -Milliseconds 350
+Start-Sleep -Milliseconds 500
 
 switch ($Action) {
   'candidate' {
@@ -152,6 +229,9 @@ $languageId = ([int64]$activeLayout) -band 0xffff
   electronProcessId = $ElectronProcessId
   windowProcessId = $windowProcessId
   windowThreadId = $windowThreadId
+  activationHresult = ('0x{0:X8}' -f ([uint32]$activationHresult))
+  textServiceClsid = '81D4E9C9-1D3B-41BC-9E6C-4B40BF79E35E'
+  profileGuid = 'FA550B04-5AD7-411F-A5AC-CA038EC515D7'
   languageId = ('0x{0:X4}' -f $languageId)
   screenshotPath = $ScreenshotPath
   timestamp = (Get-Date).ToString('o')
